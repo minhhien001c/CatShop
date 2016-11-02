4528cffc5e060aab330f9557b3835b807a2b176a
MeoMatBu-PC\MeoMatBu <minhhien001c@yahoo.com>
2016-11-02 15:27:43 +0700

    This is first commit

---
+++ CatShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatShop.Data.Infrastructure
+{
+    public class DbFactory:Disposable, IDbFactory
+    {
+        CatShopDbContext dbContext;
+        public CatShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new CatShopDbContext());
+        }
+        protected virtual void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
---
+++ CatShop.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatShop.Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory dbFactory;
+        private CatShopDbContext dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        public CatShopDbContext DbContext
+        {
+            get { return dbContext ?? (dbContext = dbFactory.Init()); }
+        }
+
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+}
