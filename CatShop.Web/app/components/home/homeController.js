93a499ee5454ff7cb9d5c7a1d8aa7ae7dd7f4a87
MeoMatBu-PC\MeoMatBu <minhhien001c@yahoo.com>
2016-11-09 09:28:42 +0700

    ui-router

---
+++ CatShop.Web/app/components/home/homeController.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="/Assets/admin/libs/angular/angular.js" />
+(function (app) {
+    app.controller('homeController', homeController);
+    function homeController()
+    { }
+})(angular.module('catshop'));
---
+++ CatShop.Web/app/shared/modules/catshop.common.js
@@ -0,0 +1,4 @@
+﻿/// <reference path="/Assets/admin/libs/angular/angular.js" />
+(function () {
+    angular.module('catshop.common', ['ui.router'])
+})();
